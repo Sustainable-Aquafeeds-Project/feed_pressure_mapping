7b1363e3fb4a8f15689571df931fdeababd5bc48
richardcottrell <richardcottrell@skbhxkwa3t8ea1.novalocal>
2022-02-09 00:25:49 +0000

    Put all repositories on RDS under 'github' umbrella, updated directories in this repo and compressed food rgns xy data to .rds and kept raw data in rdsi  raw_data folder

--- src/directories.R
+++ src/directories.R
@@ -1,3 +1,5 @@
 
-food_systems_dir <- "/mnt/rdsi/food_systems"
-food_systems_feed_dir <- "/mnt/rdsi/food_systems/feed"
+food_systems_dir <- "/mnt/rdsi/github/food_systems"
+food_systems_feed_dir <- "/mnt/rdsi/github/food_systems/feed"
+
+food_systems_data_dir <- "/mnt/rdsi/raw_data/food-systems-project"
