8c386b9a67ff4c972bdbe85a438c026955543400
cottrellr <rscottrell22@gmail.com>
2022-02-14 16:07:09 +1000

    local directories on rich's dropbox added

--- src/directories.R
+++ src/directories.R
@@ -3,3 +3,9 @@ food_systems_dir <- "/mnt/rdsi/github/food_systems"
 food_systems_feed_dir <- "/mnt/rdsi/github/food_systems/feed"
 
 food_systems_data_dir <- "/mnt/rdsi/raw_data/food-systems-project"
+
+
+
+#local directories on Rich's dropbox
+
+mapspam_local_dir <- "/Users/rsc2/Dropbox/Data/MAPSPAM"
