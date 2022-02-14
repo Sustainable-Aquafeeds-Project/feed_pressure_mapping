b61cd516091a679b808edd2bec7a19ac23e29acd
cottrellr <rscottrell22@gmail.com>
2022-02-14 15:57:29 +1000

    add local repo for Rich dropbox

--- src/directories.R
+++ src/directories.R
@@ -1,3 +1,10 @@
 
 food_systems_dir <- "/mnt/rdsi/food_systems"
 food_systems_feed_dir <- "/mnt/rdsi/food_systems/feed"
+
+
+
+#local directories (Rich's dropbox)
+
+mapspam_local_dir <- "/Users/rsc2/Dropbox/github/feed_pressure_mapping"
+
