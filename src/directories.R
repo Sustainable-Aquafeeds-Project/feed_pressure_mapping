57b7e8edfbaf429d542a947bb4910fc4510b24d5
richardcottrell <richardcottrell@skbhxkwa3t8ea1.novalocal>
2022-02-15 06:07:25 +0000

    Created new tifs with full lon lat extents for MAPSPAM data

--- src/directories.R
+++ src/directories.R
@@ -1,11 +1,9 @@
-
+#from NCEAS food-systems project
 food_systems_dir <- "/mnt/rdsi/github/food_systems"
 food_systems_feed_dir <- "/mnt/rdsi/github/food_systems/feed"
-
 food_systems_data_dir <- "/mnt/rdsi/raw_data/food-systems-project"
 
 
+#from ifpri
+mapspam_dir <- "/mnt/rdsi/raw_data/MAPSPAM"
 
-#local directories on Rich's dropbox
-
-mapspam_local_dir <- "/Users/rsc2/Dropbox/Data/MAPSPAM"
