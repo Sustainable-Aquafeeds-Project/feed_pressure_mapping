fe3b8d8c920d7f3fcf8c94be3f9d1d4b58b30883
richardcottrell <richardcottrell@skbhxkwa3t8ea1.novalocal>
2022-02-25 18:54:01 +0000

    improving parallelisation through furrr - mclapply seems to pause during run

--- src/iucn_fxs.R
+++ src/iucn_fxs.R
@@ -14,3 +14,15 @@ version <- rl_version(key=api_key)
 version <-  "2021-3" #set the version
 
 
+get_spp_api <- function(this_page){
+  spplist_url <- sprintf("https://apiv3.iucnredlist.org/api/v3/species/page/%s?token=%s", this_page,  api_key)
+  jsonlite::fromJSON(spplist_url) %>% .$result
+}
+
+
+get_habitat_api <- function(this_spp_id){
+  message(paste("processing taxonid #", this_spp_id))
+  hablist_url <- sprintf("https://apiv3.iucnredlist.org/api/v3/habitats/species/id/%s?token=%s", this_spp_id,  api_key)
+  bind_cols(jsonlite::fromJSON(hablist_url))
+  
+}
