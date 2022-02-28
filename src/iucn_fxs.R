1d2e2e8c645f37ff69899a8b9d4a762f0abf29b7
richardcottrell <richardcottrell@skbhxkwa3t8ea1.novalocal>
2022-02-28 16:44:43 +0000

    added casey's code and first few species from habitat api query that isn't quite working properly

--- src/iucn_fxs.R
+++ src/iucn_fxs.R
@@ -26,3 +26,86 @@ get_habitat_api <- function(this_spp_id){
   bind_cols(jsonlite::fromJSON(hablist_url))
   
 }
+
+
+#from casey
+
+get_from_api <- function(url, param, api_key, delay, verbose = FALSE) {
+  
+  i <- 1; tries <- 5; success <- FALSE
+  
+  while(i <= tries & success == FALSE) {
+    if(verbose) {
+      message('try #', i)
+    }
+    Sys.sleep(delay * i) ### be kind to the API server? later attempts wait longer
+    api_info <- fromJSON(sprintf(url, param, api_key)) 
+    if (class(api_info) != 'try-error') {
+      success <- TRUE
+    } else {
+      warning(sprintf('try #%s: class(api_info) = %s\n', i, class(api_info)))
+    }
+    if(verbose) {
+      message('... successful? ', success)
+    }
+    i <- i + 1
+  }
+  
+  if (class(api_info) == 'try-error') { ### multi tries and still try-error
+    api_return <- data.frame(param_id  = param,
+                             api_error = 'try-error after multiple attempts')
+  } else if (class(api_info$result) != 'data.frame') { ### result isn't data frame for some reason
+    api_return <- data.frame(param_id  = param,
+                             api_error = paste('non data.frame output: ', 
+                                               class(api_info$result), 
+                                               ' length = ', length(api_info$result)))
+  } else if (length(api_info$result) == 0) { ### result is empty
+    api_return <- data.frame(param_id  = param,
+                             api_error = 'zero length data.frame')
+  } else {
+    api_return <- api_info %>%
+      data.frame(stringsAsFactors = FALSE)
+  }
+  
+  return(api_return)
+}
+
+mc_get_from_api <- function(url, param_vec, api_key, 
+                            cores = NULL, delay = 0.5, 
+                            .id = NULL,
+                            verbose = FALSE) {
+  
+  if(is.null(cores)) {
+    numcores <- ifelse(Sys.info()[['nodename']] == 'mazu', 12, parallel::detectCores()/2)
+  } else { 
+    numcores <- cores
+  }
+  out_list <- parallel::mclapply(param_vec, 
+                                 function(x) {
+                                   get_from_api(url, x, api_key, delay, verbose)
+                                 },
+                                 mc.cores   = numcores,
+                                 mc.cleanup = TRUE) 
+  
+  if(any(sapply(out_list, class) != 'data.frame')) {
+    error_list <- out_list[sapply(out_list, class) != 'data.frame']
+    if(verbose) {
+      message('List items are not data frame: ', paste(sapply(error_list, class), collapse = '; '))
+      message('might be causing the bind_rows() error; returning the raw list instead')
+    }
+    return(out_list)
+  }
+  
+  out_df <- out_list %>%
+    setNames(param_vec) %>%
+    bind_rows(.id = 'param')
+  out_df <- out_df %>%
+    setNames(names(.) %>%
+               str_replace('result.', ''))
+  return(out_df)
+}
+
+
+
+
+
