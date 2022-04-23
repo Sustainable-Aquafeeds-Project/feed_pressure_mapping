95d5aed373804ce0a2d723a6f087f145ffa62cc8
richardcottrell <richardcottrell@richrstudio.novalocal>
2022-04-23 04:35:24 +0000

    took base raster out of src/spatial.R and saved the raster in spatial data (code in 01_tidying_data). Created new 06_exploring_biodiversity_shapefiles

--- src/spatial.R
+++ src/spatial.R
@@ -8,11 +8,3 @@ equal_area_moll_projstring <- "+proj=moll +lon_0=0 +x_0=0 +y_0=0 +ellps=WGS84 +d
 
 equal_area_moll_esri <- "ESRI:54009"
 
-
-#base rasters
-base_raster <- raster()
-values(base_raster) <- 1:ncell(base_raster)
-base_raster_ea <- projectRaster(base_raster, crs = equal_area_gp_proj)
-res(base_raster_ea) <- 10000
-values(base_raster_ea) <- 1:ncell(base_raster_ea) 
-
