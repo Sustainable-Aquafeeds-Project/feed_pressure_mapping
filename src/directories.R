0f9c0f155ab46df208e6540a140cce8175b380c7
richardcottrell <richardcottrell@richrstudio.novalocal>
2022-05-31 10:21:14 +0000

    Adds Kok et al for embodied fish amounts and the code for combining fisheries data. Fisheries catch sums to 52 million tonnes as expected.

--- src/directories.R
+++ src/directories.R
@@ -12,6 +12,12 @@ food_systems_data_dir <- "/mnt/rdsi/raw_data/food-systems-project"
 #from IFPRI
 mapspam_dir <- "/mnt/rdsi/raw_data/MAPSPAM"
 
+#watson fisheries data
+
+watson_dir <- "/mnt/rdsi/raw_data/watson_2015"
+
+
+
 #gridded livestock of the world
 glw_dir <- "/mnt/rdsi/raw_data/gridded-livestock-of-the-world"
 
@@ -21,6 +27,8 @@ iucn_dir <- "/mnt/rdsi/raw_data/iucn"
 
 
 #FAO data directories
+
+fao_dir <- "/mnt/rdsi/raw_data/fao"
 
 fishstat_dir <- "/mnt/rdsi/raw_data/fao/FAO_fishstat_2019"
 
