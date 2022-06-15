7d5c07b830482622d17037bfc06f01094ab9592c
richardcottrell <richardcottrell@richrstudio.novalocal>
2022-06-15 05:13:52 +0000

    summarises the raw GFW daily data to annual.

--- src/directories.R
+++ src/directories.R
@@ -32,11 +32,46 @@ fao_dir <- "/mnt/rdsi/raw_data/fao"
 
 fishstat_dir <- "/mnt/rdsi/raw_data/fao/FAO_fishstat_2019"
 
-#Rich local machine
-iucn_rich_local_dir <- "/Users/rsc2/Dropbox/Data/IUCN Red List"
+
+#Global fishing watch
+
+gfw_dir <- "/mnt/rdsi/raw_data/global-fishing-watch"
 
 
 
-#Rich Personal Dropbox
 
-dropbox_dir <- "/Users/rsc2/Dropbox/Data"
+###################################### RDSI backup ############################################
+
+rdsi_backup_dir <- "/Users/rsc2/OneDrive - University of Tasmania/ARC Linkage - Optimising aquafeeds/rdsi"
+
+
+#from NCEAS food-systems project
+food_systems_backup_backup_dir <- "/Users/rsc2/OneDrive - University of Tasmania/ARC Linkage - Optimising aquafeeds/rdsi/github/food_systems"
+food_systems_feed_backup_dir <- "/Users/rsc2/OneDrive - University of Tasmania/ARC Linkage - Optimising aquafeeds/rdsi/github/food_systems/feed"
+food_systems_data_backup_dir <- "/Users/rsc2/OneDrive - University of Tasmania/ARC Linkage - Optimising aquafeeds/rdsi/raw_data/food-systems-project"
+
+
+#from IFPRI
+mapspam_backup_dir <- "/Users/rsc2/OneDrive - University of Tasmania/ARC Linkage - Optimising aquafeeds/rdsi/raw_data/MAPSPAM"
+
+#watson fisheries data
+
+watson_backup_dir <- "/Users/rsc2/OneDrive - University of Tasmania/ARC Linkage - Optimising aquafeeds/rdsi/raw_data/watson_2015"
+
+
+
+#gridded livestock of the world
+glw_backup_dir <- "/Users/rsc2/OneDrive - University of Tasmania/ARC Linkage - Optimising aquafeeds/rdsi/raw_data/gridded-livestock-of-the-world"
+
+
+#IUCN Redlist data
+iucn_backup_dir <- "/Users/rsc2/OneDrive - University of Tasmania/ARC Linkage - Optimising aquafeeds/rdsi/raw_data/iucn"
+
+
+#FAO data directories
+
+fao_backup_dir <- "/Users/rsc2/OneDrive - University of Tasmania/ARC Linkage - Optimising aquafeeds/rdsi/raw_data/fao"
+
+fishstat_backup_dir <- "/Users/rsc2/OneDrive - University of Tasmania/ARC Linkage - Optimising aquafeeds/rdsi/raw_data/fao/FAO_fishstat_2019"
+
+
