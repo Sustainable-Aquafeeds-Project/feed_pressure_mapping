ce4a15df5d2caa5c4be732594f4ba8ede74ee605
richardcottrell <richardcottrell@richrstudio.novalocal>
2022-07-06 07:13:56 +0000

    Starts to join the parker data gears to the embodide fish to match up the emission intensities

--- src/directories.R
+++ src/directories.R
@@ -6,8 +6,7 @@ rdsi_raw_data_dir <- "/mnt/rdsi/raw_data"
 
 
 #from NCEAS food-systems project
-food_systems_dir <- "/mnt/rdsi/github/food_systems"
-food_systems_feed_dir <- "/mnt/rdsi/github/food_systems/feed"
+food_systems_feed_dir <- "/mnt/rdsi/github/raw_data/food_systems/feed"
 food_systems_data_dir <- "/mnt/rdsi/raw_data/food-systems-project"
 
 
